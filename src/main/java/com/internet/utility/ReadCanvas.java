38af50938831e1aeab1ef4a2b8299317d3b37a29
sahuShashank12 <sahu.shashank.12@gmail.com>
2021-08-21 02:12:19 +0530

    Shashank: Moved Canvas Image under resources folder

--- src/main/java/com/internet/utility/ReadCanvas.java
+++ src/main/java/com/internet/utility/ReadCanvas.java
@@ -8,11 +8,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.io.FileHandler;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class ReadCanvas {
 
-    /*The Tessaract Library should be installed on the System in order to use Tess4j on MacOS and
+    /*The Tessaract Library should be installed on the System in order to run the use Tess4j on MacOS and
     * Some setting needs to be done additionally to add these lib of Tessaract to Tess4j file path
     * cp /usr/local/Cellar/tesseract/4.1.1/lib/libtesseract.4.dylib darwin/libtesseract.dylib
     * */
@@ -27,13 +28,12 @@ public class ReadCanvas {
         } catch (TesseractException e) {
             e.printStackTrace();
         }
-        System.out.println(imageText);
         return imageText;
     }
 
     public String captureCanvasImage(WebElement element){
         File src = element.getScreenshotAs(OutputType.FILE);
-        String path = System.getProperty("screenshots/canvas.png");
+        String path = "src/test/resources/screenshots/canvas.png";
         File canvasImage = new File(path);
         try {
             FileHandler.copy(src,canvasImage);
